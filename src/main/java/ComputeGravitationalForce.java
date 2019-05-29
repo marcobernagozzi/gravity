36f803630e0dd0d94ac08dcc950697b3448bc5bd
marcobernagozzi <marco.bernagozzi@gmail.com>
2019-05-29 16:47:37 +0200

    Remove redundant operations

--- src/main/java/ComputeGravitationalForce.java
+++ src/main/java/ComputeGravitationalForce.java
@@ -1,5 +1,4 @@
 
-import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -10,13 +9,13 @@ import java.util.concurrent.TimeUnit;
 public class ComputeGravitationalForce {
     public final static double G = 6.67408 * 10E-11;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         double mass = 1E10;
         Planet[] planets = new Planet[]{
-            new Planet(300, 10, mass),
-            new Planet(10, 500, mass),
-            /*new Planet(10, distance, mass),*/
-            new Planet(500, 700, mass),
+                new Planet(300, 10, mass),
+                new Planet(10, 500, mass),
+                /*new Planet(10, distance, mass),*/
+                new Planet(500, 700, mass),
         };
 
         int n = planets.length;
@@ -24,7 +23,7 @@ public class ComputeGravitationalForce {
         double interval = 1; // in seconds
         double[][][] accelerations = new double[n][n][2];
 
-        while(true) {
+        while (true) {
             ExecutorService exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
             for (int i = 0; i < n; i++) {
                 for (int j = i + 1; j < n; j++) {
@@ -103,24 +102,22 @@ public class ComputeGravitationalForce {
             Planet planetB = planets[b];
             double deltaX = planetA.x - planetB.x;
             double deltaY = planetA.y - planetB.y;
-            double distance = Math.sqrt(Math.pow(deltaY, 2) + Math.pow(deltaX, 2));
-            double force = G * planetA.mass * planetB.mass / (Math.pow(distance, 2));
-
-            double accelerationA = force / planetA.mass;
-            double accelerationB = force / planetB.mass;
-            if(deltaY != 0) {
+            double forceOverMassSquared = G / (Math.pow(deltaY, 2) + Math.pow(deltaX, 2));
+            double accelerationA = forceOverMassSquared * planetB.mass;
+            double accelerationB = forceOverMassSquared * planetA.mass;
+            if (deltaY != 0) {
                 double accelerationAY = accelerationA * Math.sqrt(1 / (1 + Math.pow(deltaX / deltaY, 2)));
                 double accelerationAX = accelerationAY * Math.abs(deltaX / deltaY);
                 double accelerationBY = accelerationB * Math.sqrt(1 / (1 + Math.pow(deltaX / deltaY, 2)));
                 double accelerationBX = accelerationBY * Math.abs(deltaX / deltaY);
                 accelerations[a][b][0] = accelerationAX * (deltaX > 0 ? -1 : 1);
                 accelerations[a][b][1] = accelerationAY * (deltaY > 0 ? -1 : 1);
-                accelerations[b][a][0] = accelerationBX  * (deltaX < 0 ? -1 : 1);
-                accelerations[b][a][1] = accelerationBY  * (deltaY < 0 ? -1 : 1);
+                accelerations[b][a][0] = accelerationBX * (deltaX < 0 ? -1 : 1);
+                accelerations[b][a][1] = accelerationBY * (deltaY < 0 ? -1 : 1);
             } else {
-                accelerations[a][b][0] = accelerationA  * (deltaX > 0 ? -1 : 1);
+                accelerations[a][b][0] = accelerationA * (deltaX > 0 ? -1 : 1);
                 accelerations[a][b][1] = 0;
-                accelerations[b][a][0] = accelerationB  * (deltaX < 0 ? -1 : 1);
+                accelerations[b][a][0] = accelerationB * (deltaX < 0 ? -1 : 1);
                 accelerations[b][a][1] = 0;
             }
         }
--- src/main/java/PlotHelper.java
+++ src/main/java/PlotHelper.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
