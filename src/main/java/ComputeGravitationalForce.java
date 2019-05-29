c210a7c386edede8caee087dabfc5f45d8996c61
marcobernagozzi <marco.bernagozzi@gmail.com>
2019-05-29 17:51:56 +0200

    Ignore singularities

--- src/main/java/ComputeGravitationalForce.java
+++ src/main/java/ComputeGravitationalForce.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -9,13 +10,14 @@ import java.util.concurrent.TimeUnit;
 public class ComputeGravitationalForce {
     public final static double G = 6.67408 * 10E-11;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         double mass = 1E10;
         Planet[] planets = new Planet[]{
-                new Planet(300, 10, mass),
-                new Planet(10, 500, mass),
-                /*new Planet(10, distance, mass),*/
-                new Planet(500, 700, mass),
+            new Planet(300, 10, mass),
+            new Planet(10, 500, mass),
+            new Planet(100, 300, mass),
+            /*new Planet(10, distance, mass),*/
+            new Planet(500, 700, mass),
         };
 
         int n = planets.length;
@@ -23,7 +25,7 @@ public class ComputeGravitationalForce {
         double interval = 1; // in seconds
         double[][][] accelerations = new double[n][n][2];
 
-        while (true) {
+        while(true) {
             ExecutorService exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
             for (int i = 0; i < n; i++) {
                 for (int j = i + 1; j < n; j++) {
@@ -102,6 +104,19 @@ public class ComputeGravitationalForce {
             Planet planetB = planets[b];
             double deltaX = planetA.x - planetB.x;
             double deltaY = planetA.y - planetB.y;
+            double lim = 5;
+            if(deltaX > 0 && deltaX < lim) {
+                deltaX = lim;
+            }
+            if(deltaY > 0 && deltaY < lim) {
+                deltaY = lim;
+            }
+            if(deltaX < 0 && deltaX > -lim) {
+                deltaX = -lim;
+            }
+            if(deltaY < 0 && deltaY > -lim) {
+                deltaY = -lim;
+            }
             double forceOverMassSquared = G / (Math.pow(deltaY, 2) + Math.pow(deltaX, 2));
             double accelerationA = forceOverMassSquared * planetB.mass;
             double accelerationB = forceOverMassSquared * planetA.mass;
@@ -112,12 +127,12 @@ public class ComputeGravitationalForce {
                 double accelerationBX = accelerationBY * Math.abs(deltaX / deltaY);
                 accelerations[a][b][0] = accelerationAX * (deltaX > 0 ? -1 : 1);
                 accelerations[a][b][1] = accelerationAY * (deltaY > 0 ? -1 : 1);
-                accelerations[b][a][0] = accelerationBX * (deltaX < 0 ? -1 : 1);
-                accelerations[b][a][1] = accelerationBY * (deltaY < 0 ? -1 : 1);
+                accelerations[b][a][0] = accelerationBX  * (deltaX < 0 ? -1 : 1);
+                accelerations[b][a][1] = accelerationBY  * (deltaY < 0 ? -1 : 1);
             } else {
-                accelerations[a][b][0] = accelerationA * (deltaX > 0 ? -1 : 1);
+                accelerations[a][b][0] = accelerationA  * (deltaX > 0 ? -1 : 1);
                 accelerations[a][b][1] = 0;
-                accelerations[b][a][0] = accelerationB * (deltaX < 0 ? -1 : 1);
+                accelerations[b][a][0] = accelerationB  * (deltaX < 0 ? -1 : 1);
                 accelerations[b][a][1] = 0;
             }
         }
